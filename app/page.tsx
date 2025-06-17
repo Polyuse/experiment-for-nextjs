497b94931cb1adc22bf6be05de8bd900bbe87cae
hnmwt <wataru.homma@polyuse.xyz>
2025-06-17 20:21:51 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
 import Image from "next/image";
 
+
+
 export default function Home() {
+
+  const aaa=process.env.NEXT_PUBLIC_SUPABASE_URL
+  const log =()=>{
+    console.log('ddd::',aaa)
+  }
+   log()
   return (
+
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
@@ -12,6 +21,7 @@ export default function Home() {
           height={38}
           priority
         />
+       
         <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
