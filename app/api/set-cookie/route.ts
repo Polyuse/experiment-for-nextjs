23ff19aa4af7df2a48bc63d31f92f16afdc7e003
hnmwt <wataru.homma@polyuse.xyz>
2025-06-18 10:39:09 +0900

    tokenidをクッキーにセット

--- app/api/set-cookie/route.ts
+++ app/api/set-cookie/route.ts
@@ -4,13 +4,12 @@ import { NextResponse } from "next/server";
 export async function POST(request: Request) {
   const cookieStore = cookies();
   const body = await request.json();
-  const { id } = body;
+  const { id, name } = body;
   console.log("dddd::", request);
 
-  (await cookieStore).set("next_org_id", id, {
+  (await cookieStore).set(name, id, {
     httpOnly: true,
-    path: "/",
-    maxAge: 60 * 60 * 24, // 1日
+    path: "/test",
   });
 
   return NextResponse.json({ message: "Cookie set" });
--- app/login/actions.ts
+++ app/login/actions.ts
@@ -1,45 +1,55 @@
-'use server'
+"use server";
 
-import { revalidatePath } from 'next/cache'
-import { redirect } from 'next/navigation'
+import { revalidatePath } from "next/cache";
+import { redirect } from "next/navigation";
 
-import { createClient } from '@/utils/supabase/server'
+import { createClient } from "@/utils/supabase/server";
+import { cookies } from "next/headers";
 
 export async function login(formData: FormData) {
-  const supabase = await createClient()
+  const supabase = await createClient();
 
   // type-casting here for convenience
   // in practice, you should validate your inputs
-  const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
+  const signInData = {
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
 
-  const { error } = await supabase.auth.signInWithPassword(data)
+  const { data, error } = await supabase.auth.signInWithPassword(signInData);
 
   if (error) {
-    redirect('/error')
+    redirect("/error");
   }
-
-  revalidatePath('/', 'layout')
-  redirect('/account')
+  if (data.session) {
+    console.log("cookie::", data.session.access_token);
+    const cookieStore = cookies();
+    (await cookieStore).set("token_id", data?.session.access_token, {
+      httpOnly: true,
+      path: "/",
+    });
+  } else {
+    console.log("not cookie::");
+  }
+  revalidatePath("/", "layout");
+  redirect("/account");
 }
 
 export async function signup(formData: FormData) {
-  const supabase = await createClient()
+  const supabase = await createClient();
   // type-casting here for convenience
   // in practice, you should validate your inputs
   const data = {
-    email: formData.get('email') as string,
-    password: formData.get('password') as string,
-  }
-console.log('eeee',)
-  const { error } = await supabase.auth.signUp(data)
+    email: formData.get("email") as string,
+    password: formData.get("password") as string,
+  };
+
+  const { error } = await supabase.auth.signUp(data);
 
   if (error) {
-    redirect('/error')
+    redirect("/error");
   }
 
-  revalidatePath('/', 'layout')
+  revalidatePath("/", "layout");
   // redirect('/account')
 }
