60afbb4510e2d9e5bcdf118c777e65ab58f68662
hnmwt <wataru.homma@polyuse.xyz>
2025-06-18 14:12:35 +0900

    fix

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -2,5 +2,25 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ message: "Hello from Cloud Run API!" });
+  switch (req.method) {
+    case "GET":
+      // Read（読み取り）
+      res.status(200).json({ message: "Fetching data..." });
+      break;
+    case "POST":
+      // Create（新規作成）
+      res.status(201).json({ message: "Data created" });
+      break;
+    case "PUT":
+      // Update（更新）
+      res.status(200).json({ message: "Data updated" });
+      break;
+    case "DELETE":
+      // Delete（削除）
+      res.status(200).json({ message: "Data deleted" });
+      break;
+    default:
+      res.setHeader("Allow", ["GET", "POST", "PUT", "DELETE"]);
+      res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
 }
