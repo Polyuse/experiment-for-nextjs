04ec4af912029e5921e5bb6895a50403ae52f88e
hnmwt <wataru.homma@polyuse.xyz>
2025-06-20 10:59:06 +0900

    123をplus

--- app/page.tsx
+++ app/page.tsx
@@ -4,15 +4,16 @@ export default function Home() {
   return (
     <div className="row">
       <div className="col-12">
-        <h1 className="header">Supabase Auth + Storage</h1>
+        <h1 className="header">Supabase Auth + Storage123</h1>
         <p>
-          Experience our Auth and Storage through a simple profile management example. Create a user
-          profile and upload an avatar image. Fast, simple, secure.
+          Experience our Auth and Storage through a simple profile management
+          example. Create a user profile and upload an avatar image. Fast,
+          simple, secure.
         </p>
       </div>
       <div className="col-6 form-widget">
         <Link href="/login">Auth page</Link>
       </div>
     </div>
-  )
+  );
 }
